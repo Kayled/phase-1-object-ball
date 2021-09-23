918dc485112e6f69f527ee9c4b70c4600bde5c59
+ <+>
2021-09-23 16:24:27 -0700

    is it 5 yet

---
+++ src/00-objectball.js
@@ -0,0 +1,56 @@
+function homeTeamName() {
+    let object['home']['teamName']{
+    home:{
+        teamName:["Brooklyn Nets"], 
+        color:["Black, White"],
+        players:[
+            {name:"Alan Anderson",
+                "number": 0 ,
+                "shoe": 16,
+                "points": 22,
+                "rebounds": 12,
+                "assists": 12,
+                "steals": 3,
+                "blocks": 1,
+                "slamDunks": 1,
+            },
+            {name:"Alan Anderson",
+                "number": 0 ,
+                "shoe": 16,
+                "points": 22,
+                "rebounds": 12,
+                "assists": 12,
+                "steals": 3,
+                "blocks": 1,
+                "slamDunks": 1,
+            },
+            {name:"Alan Anderson",
+                "number": 0 ,
+                "shoe": 16,
+                "points": 22,
+                "rebounds": 12,
+                "assists": 12,
+                "steals": 3,
+                "blocks": 1,
+                "slamDunks": 1,
+            },
+        ]
+
+
+
+
+
+    }
+    },
+// console.log (gameObjects)
+
+
+    // away:{
+    //     teamName:
+    //     color:
+    //     players:
+
+    // }
+}
+
+console.log (gameObject);
