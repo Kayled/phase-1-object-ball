1793ee8c475a117057f30fe91b6b373ee4eb4737
Katherine Iannaccone <katherineiannaccone@yahoo.com>
2021-09-23 17:06:22 -0600

    kats version of object ball lab

--- src/00-objectball.js
+++ src/00-objectball.js
@@ -5,7 +5,7 @@ function homeTeamName() {
         color:["Black, White"],
         players:[
             {name:"Alan Anderson",
-                "number": 0 ,
+                "number": 0,
                 "shoe": 16,
                 "points": 22,
                 "rebounds": 12,
@@ -14,43 +14,109 @@ function homeTeamName() {
                 "blocks": 1,
                 "slamDunks": 1,
             },
-            {name:"Alan Anderson",
-                "number": 0 ,
-                "shoe": 16,
-                "points": 22,
+            {name:"Reggie Evans",
+                "number": 30 ,
+                "shoe": 14,
+                "points": 12,
                 "rebounds": 12,
                 "assists": 12,
-                "steals": 3,
-                "blocks": 1,
-                "slamDunks": 1,
+                "steals": 12,
+                "blocks": 12,
+                "slamDunks": 7,
             },
-            {name:"Alan Anderson",
-                "number": 0 ,
-                "shoe": 16,
-                "points": 22,
-                "rebounds": 12,
-                "assists": 12,
+            {name:"Brook Lopez",
+                "number": 11,
+                "shoe": 17,
+                "points": 17,
+                "rebounds": 19,
+                "assists": 10,
                 "steals": 3,
                 "blocks": 1,
+                "slamDunks": 15,
+            },
+            {name:"Mason Plumlee",
+                "number": 1 ,
+                "shoe": 19,
+                "points": 26,
+                "rebounds": 12,
+                "assists": 6,
+                "steals": 3,
+                "blocks": 8,
+                "slamDunks": 5,
+            },
+            {name:"Jason Terry",
+                "number": 31 ,
+                "shoe": 15,
+                "points": 19,
+                "rebounds": 2,
+                "assists": 2,
+                "steals": 4,
+                "blocks": 11,
                 "slamDunks": 1,
             },
         ]
 
-
-
-
-
     }
-    },
-// console.log (gameObjects)
+},
 
-
-    // away:{
-    //     teamName:
-    //     color:
-    //     players:
-
-    // }
-}
+function awayTeamName() {
+    let object['away']['teamName']{
+    away:{
+        teamName:["Charlotte Hornets"], 
+        color:["Turquoise, Purple"],
+        players:[
+            {name:"Jeff Adrien",
+                "number": 4,
+                "shoe": 18,
+                "points": 10,
+                "rebounds": 1,
+                "assists": 1,
+                "steals": 2,
+                "blocks": 7,
+                "slamDunks": 2,
+            },
+            {name:"Bismak Biyombo",
+                "number": 0 ,
+                "shoe": 16,
+                "points": 12,
+                "rebounds": 4,
+                "assists": 7,
+                "steals": 7,
+                "blocks": 15,
+                "slamDunks": 10,
+            },
+            {name:"DeSagna Diop",
+                "number": 2,
+                "shoe": 14,
+                "points": 24,
+                "rebounds": 12,
+                "assists": 12,
+                "steals": 4,
+                "blocks": 5,
+                "slamDunks": 5,
+            },
+            {name:"Ben Gordon",
+                "number": 8 ,
+                "shoe": 15,
+                "points": 33,
+                "rebounds": 3,
+                "assists": 2,
+                "steals": 1,
+                "blocks": 1,
+                "slamDunks": 0,
+            },
+            {name:"Brendan Haywood",
+                "number": 33,
+                "shoe": 15,
+                "points": 6,
+                "rebounds": 12,
+                "assists": 12,
+                "steals": 22,
+                "blocks": 5,
+                "slamDunks": 12,
+            }, 
+        ]
+    }
+};
 
 console.log (gameObject);
